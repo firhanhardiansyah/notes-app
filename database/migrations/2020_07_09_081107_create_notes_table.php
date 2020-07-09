cf57a2b7fc0d6694c57bf6122bbf35bc86053899
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2020-07-09 17:01:25 +0700

    [Repair] notes_id -> id

--- database/migrations/2020_07_09_081107_create_notes_table.php
+++ database/migrations/2020_07_09_081107_create_notes_table.php
@@ -14,7 +14,7 @@ class CreateNotesTable extends Migration
     public function up()
     {
         Schema::create('notes', function (Blueprint $table) {
-            $table->bigIncrements('notes_id');
+            $table->bigIncrements('id');
             $table->string('title');
             $table->text('description');
             $table->timestamps();
